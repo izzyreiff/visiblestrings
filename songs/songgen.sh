3ca2933469139624c8841672516fe18e77f3f66c
Izzy <izzyreiff@gmail.com>
2023-09-15 00:16:17 -0400

    Completed Debut

---
+++ songs/songgen.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# TS1 - Debut
+for i in {8..14}; do
+    touch "1_$i.txt"
+done
+# TS2 - Fearless + TV
+# for i in {1..25}; do
+#     touch "2_$i.txt"
+# done
+# # TS3 - Speak Now + TV
+# for i in {1..22}; do
+#     touch "3_$i.txt"
+# done
+# # TS4 - Red + TV
+# for i in {1..29}; do
+#     touch "4_$i.txt"
+# done
+# # TS5 - 1989
+# for i in {1..16}; do
+#     touch "5_$i.txt"
+# done
+# # TS6 - reputation
+# for i in {1..15}; do
+#     touch "6_$i.txt"
+# done
+# # TS7 - Lover
+# for i in {1..18}; do
+#     touch "7_$i.txt"
+# done
+# # TS8 - folklore
+# for i in {1..17}; do
+#     touch "8_$i.txt"
+# done
+# # TS9 - evermore
+# for i in {1..17}; do
+#     touch "9_$i.txt"
+# done
+# # TS10 - Midnights
+# for i in {1..22}; do
+#     touch "10_$i.txt"
+# done
