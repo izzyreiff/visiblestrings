37e933ebd66e94b69910900aac3e49575159b196
Izzy <izzyreiff@gmail.com>
2023-09-14 23:24:43 -0400

    Added dictionary

--- dictionary.py
+++ dictionary.py
@@ -4,5 +4,14 @@ dict = {
     "Picture To Burn": "1_2.txt",
     "Teardrops on My Guitar": "1_3.txt",
     "A Place In This World": "1_4.txt",
-    "Cold As You": "1_5.txt"
+    "Cold As You": "1_5.txt",
+    "The Outside": "1_6.txt",
+    "Tied Together with a Smile": "1_7.txt",
+    "Stay Beautiful": "1_8.txt",
+    "Should've Said No": "1_9.txt",
+    "Mary's Song (Oh My My My)": "1_10.txt",
+    "Our Song": "1_11.txt",
+    "I'm Only Me When I'm With You": "1_12.txt",
+    "Invisible": "1_13.txt",
+    "A Perfectly Good Heart": "1_14.txt"
 }
