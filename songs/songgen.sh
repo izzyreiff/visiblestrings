4d6908c37c92f63e4073e7966586221ecd2f9dc8
Izzy <izzyreiff@gmail.com>
2023-09-16 22:00:30 -0400

    Update

--- songs/songgen.sh
+++ songs/songgen.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# TS1 - Debut
-for i in {8..14}; do
-    touch "1_$i.txt"
-done
-# TS2 - Fearless + TV
+# TS1 - Debut - DONE
+# for i in {1..14}; do
+#     touch "1_$i.txt"
+# done
+# TS2 - Fearless + TV - DONE
 # for i in {1..25}; do
 #     touch "2_$i.txt"
 # done
-# # TS3 - Speak Now + TV
+# TS3 - Speak Now + TV
 # for i in {1..22}; do
 #     touch "3_$i.txt"
 # done
-# # TS4 - Red + TV
-# for i in {1..29}; do
+# TS4 - Red + TV
+# for i in {1..30}; do
 #     touch "4_$i.txt"
 # done
-# # TS5 - 1989
-# for i in {1..16}; do
-#     touch "5_$i.txt"
-# done
+# TS5 - 1989
+for i in {1..16}; do
+    touch "5_$i.txt"
+done
 # # TS6 - reputation
 # for i in {1..15}; do
 #     touch "6_$i.txt"
